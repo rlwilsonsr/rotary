1ef528b924c505912092278df19873111556d3d2
Rick Wilson <rlwilsonsr@users.noreply.github.com>
2016-01-13 22:50:00 -0600

    Update project.clj
    Incrementing versions of dependencies to latest.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject rotary "0.4.1"
+(defproject rotary "0.4.2"
   :description "Amazon DynamoDB API"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/algo.generic "0.1.0"]
-                 [org.clojure/core.incubator "0.1.0"]
-                 [com.amazonaws/aws-java-sdk "1.4.3"]]
-  :plugins [[codox "0.6.4"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/algo.generic "0.1.2"]
+                 [org.clojure/core.incubator "0.1.3"]
+                 [com.amazonaws/aws-java-sdk "1.10.46"]]
+  :plugins [[codox "0.9.1"]])
